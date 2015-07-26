08f6c99e5cd26953a84aa087b973fd5c1a9c0f8c
Eibriel <eibriel@yahoo.com.ar>
2015-07-26 19:43:27 -0300

    Adding basic chat capabilities

---
+++ board_hello.py
@@ -0,0 +1,124 @@
+board_hello = {
+    'name': 'hello',
+    'triggers': ['first_hi'],
+    'nodes': [
+        {
+            'name': 'good_day',
+            'text': [['¡Buen día!'], ['¡Buenos dias!']],
+            'decoration': ['repeat_last_char'],
+            'children': ['hello']
+        },
+        {
+            'name': 'hello',
+            'text': [['¡Hola!']],
+            'children': ['who_are_you', 'what_is_hovyu', 'what_is_sustainable', 'where_cani_find_sustainable_stores', 'i_know_a_sustainable_store']
+        },
+        {
+            'name': 'who_are_you',
+            'text': [['¿Quién sos?']],
+            'children': ['i_am_hovyu'],
+            'if_not': ['know_hovyubot']
+        },
+        {
+            'name': 'what_is_hovyu',
+            'text': [['¿Qué significa Hovyũ?']],
+            'children': ['hovyu_means'],
+            'if': ['know_hovyubot'],
+            'if_not': ['know_hovyu']
+        },
+        {
+            'name': 'i_am_hovyu',
+            'text': [['Soy Hovyũ, un bot verde', 'me interesa la sostenibilidad', 'si a vos también te interesa podemos charlar!']],
+            'children': ['what_is_hovyu', 'what_is_sustainable', 'where_cani_find_sustainable_stores', 'i_know_a_sustainable_store'],
+            'infer': ['know_hovyubot']
+        },
+        {
+            'name': 'hovyu_means',
+            'text': [['Hovyũ significa Verde en guaraní', 'se pronuncia Hoviú o Hovuú']],
+            'children': ['who_are_you', 'what_is_sustainable', 'where_cani_find_sustainable_stores', 'i_know_a_sustainable_store'],
+            'infer': ['know_hovyu']
+        },
+        {
+            'name': 'what_is_sustainable',
+            'text': [['¿Qué es sostenible?']],
+            'children': ['sustainibility_is'],
+            'if_not': ['know_sustainability']
+        },
+        {
+            'name': 'where_cani_find_sustainable_stores',
+            'text': [["¿Dónde encuentro comercios sostenibles?"]],
+            'children': ['send_me_your_location_and_i_tell_you_green_stores_in_the_zone'],
+            'if': ['know_sustainability']
+        },
+        {
+            'name': 'i_know_a_sustainable_store',
+            'text': [["Yo conozco un comercio sostenible"]],
+            'children': ['great_join_the_group_and_comment_it'],
+            'if': ['know_sustainability']
+        },
+        {
+            'name': 'sustainibility_is',
+            'text': [
+                [
+                    'Sostenibilidad (a veces llamado Sustentabilidad) conciste en satisfacer las necesidades de la actual generación sin sacrificar la capacidad de futuras generaciones de satisfacer sus propias necesidades.',
+                    'Sueno como Wikipedia! 😜'
+                ]
+            ],
+            'children': ['i_understand', 'i_dont_understand_sustainibility']
+        },
+        {
+            'name': 'sustainibility_is_2',
+            'text': [
+                ['Es promover el progreso económico y social respetando los ecosistemas naturales y la calidad del medio ambiente.'],
+                ['Hay que adoptar valores para mantener valores armónicos y satisfactorios de educación, capacitación y concientización.']],
+            'children': ['i_understand', 'i_understand_not']
+        },
+        {
+            'name': 'send_me_your_location_and_i_tell_you_green_stores_in_the_zone',
+            'text': [
+                [
+                    'Enviame tu hubicación y te digo que comercios verdes hay por tu zona'
+                ]
+            ],
+            'children': ['i_understand']
+        },
+        {
+            'name': 'great_join_the_group_and_comment_it',
+            'text': [
+                [
+                    'Genial! Unite al grupo https://telegram.me/joinchat/0338811e00225f1561463d99065a12d7 y comentálo!'
+                ]
+            ],
+            'children': ['i_understand']
+        },
+        {
+            'name': 'i_understand',
+            'text': [['¡Entendido!']],
+            'children': ['great'],
+            'infer': ['know_sustainability']
+        },
+        {
+            'name': 'i_dont_understand_sustainibility',
+            'text': [['Sigo sin entender']],
+            'children': ['sustainibility_is_2']
+        },
+        {
+            'name': 'i_understand_not',
+            'text': [['Entiendo... si... perfectamente...']],
+            'children': ['dont_mok_me']
+        },
+
+        {
+            'name': 'great',
+            'text': [['¡Genial!'], ['¡Perfecto!']],
+            'children': ['who_are_you', 'what_is_hovyu', 'what_is_sustainable', 'where_cani_find_sustainable_stores', 'i_know_a_sustainable_store']
+        },
+        {
+            'name': 'dont_mok_me',
+            'text': [['¡No te burles! 😡'], ['Si... seguro... 😒']],
+            'children': ['who_are_you', 'what_is_hovyu', 'what_is_sustainable', 'where_cani_find_sustainable_stores', 'i_know_a_sustainable_store']
+        }
+
+
+    ]
+}
--- hovyubot.py
+++ hovyubot.py
@@ -1,47 +1,171 @@
 #!/bin/python3
 
+import json
+import random
 import requests
 from config import Config
 
-last_update = 0
-bot_token = Config.bot_token
-server_url = Config.server_url
+from board_hello import board_hello
+
+class bot:
+    last_update = 0
+    bot_token = Config.bot_token
+    server_url = Config.server_url
+
+    last = ''
+
+    infers = []
+
+    def send_to_bot(self, access_point, data=None):
+        try:
+            r = requests.get('https://api.telegram.org/bot{0}/{1}'.format(self.bot_token, access_point), data=data)
+        except requests.exceptions.ConnectionError:
+            print ("Connection Error")
+            return None
+        return r
+
+    def send_to_widu(self, access_point, data=None):
+        try:
+            r = requests.get('{0}/{1}'.format(self.server_url, access_point), data=data)
+        except requests.exceptions.ConnectionError:
+            print ("Connection Error")
+            return None
+        return r
+
+    def bot_loop(self):
+        while 1:
+            r = self.send_to_bot('getUpdates?timeout=30&offset={0}'.format(self.last_update))
+            if not r:
+                continue
+            r_json = r.json()
+            #print (r_json)
+            if not r_json['ok']:
+                break
+            for result in r_json['result']:
+                msgs = []
+                children = None
+                infer = None
+                if_not = None
+                keys = []
+
+                if result['update_id'] >= self.last_update:
+                    self.last_update = result['update_id'] + 1
+                chat_id = result['message']['chat']['id']
+
+                # Location:
+                if 'location' in result['message']:
+                    longitude = result['message']['location']['longitude']
+                    latitude = result['message']['location']['latitude']
+                    r = self.send_to_widu('stores?product=&activity=&latitude={0}&longitude={1}&page=1'.format(latitude, longitude))
+                    #print (r.text)
+                    stores = r.json()['_items']
+                    if len(stores) == 0:
+                        msg = 'Ahora no recuerdo ningún comercio verde por esa zona, pero si me llego a acordar te aviso! 😊'
+                    else:
+                        if len(stores) == 1:
+                            msg = 'Sé de un comercio verde por tu zona:\n'
+                        else:
+                            msg = 'Sé de {0} comercios verdes por tu zona 😃\n'.format(len(stores))
+                        for store in stores:
+                            name = store['name']
+                            description =  store['description']
+                            address = store['address']
+                            distance_klm = store['distance_klm']
+                            if distance_klm < 1:
+                                time_bike = '{0} minutos a pie'.format(int(round(distance_klm*9)))
+                            else:
+                                time_bike = '{0} minutos en bicicleta'.format(int(round(180/distance_klm)))
+                            if address == '':
+                                address = '(No recuerdo la dirección 😶, deberás preguntar en el barrio)'
+                            msg = '{0}"{1}"\n{2}\n{3}\n{4}\n\n'.format(msg, name, description, address, time_bike)
+                    msgs = [msg]
+
+                # Text
+                if 'text' in result['message']:
+                    text = result['message']['text']
+                    if text == '/start':
+                        text = '¡Buen día!'
+                    elif text[0] == '/':
+                        text = text[1:]
+                    elif text[0:9] == '@HovyuBot ':
+                        text = text[10:]
+                    for node in board_hello['nodes']:
+                        for node_text in node['text']:
+                            if [text] == node_text:
+                                if 'children' in node:
+                                    children = node['children']
+                                if 'infer' in node:
+                                    infer = node['infer']
+                                break
+                        if children:
+                            break
+                    #print (infer)
+                    if infer:
+                        self.infers = list(set(self.infers + infer))
+                    #print (self.infers)
+                    if children:
+                        children_name = random.choice(children) # TODO No Random
+                        for node in board_hello['nodes']:
+                            if node['name']==children_name:
+                                child = node
+                                break
+                        msgs = random.choice(child['text'])
+                        if 'infer' in child:
+                            infer = node['infer']
+                            self.infers = list(set(self.infers + infer))
+                        if 'children' in child:
+                            for second_child in child['children']:
+                                for node in board_hello['nodes']:
+                                    if node['name']==second_child:
+                                        dont_show = False
+                                        if 'if_not' in node:
+                                            #print (node['if_not'])
+                                            #print (self.infers)
+                                            for not_ in node['if_not']:
+                                                if not_ in self.infers:
+                                                    dont_show = True
+                                                    break
+                                        show = False
+                                        if 'if' in node:
+                                            for yes in node['if']:
+                                                if yes in self.infers:
+                                                    show = True
+                                                    break
+                                        else:
+                                            show = True
+                                        #print (node['name'])
+                                        #print ('SHOW {0}'.format(show))
+                                        #print ('DONT SHOW {0}'.format(dont_show))
+                                        if show and not dont_show:
+                                            keys.append([random.choice(node['text'])[0]])
+                                        break
+
+                keyboard = {
+                    "keyboard": keys,
+                    "resize_keyboard": True
+                }
+                for  msg in msgs:
+                    data = {
+                        'chat_id': chat_id,
+                        'action': 'typing'
+                    }
+                    r = self.send_to_bot('sendChatAction', data = data)
+                    data = {
+                        'chat_id': chat_id,
+                        'text': msg,
+                    }
+                    #print (keys)
+                    if len(keys)>0:
+                        data['reply_markup'] = json.dumps(keyboard)
+                    r = self.send_to_bot('sendMessage', data = data)
+
+Bot = bot()
 
 while 1:
-    r = requests.get('https://api.telegram.org/bot{0}/getUpdates?timeout=30&offset={1}'.format(bot_token, last_update))
-    r_json = r.json()
-    #print (r_json)
-    for result in r_json['result']:
-        msg = ''
-        if result['update_id'] >= last_update:
-            last_update = result['update_id'] + 1
-        chat_id = result['message']['chat']['id']
-        if 'location' in result['message']:
-            longitude = result['message']['location']['longitude']
-            latitude = result['message']['location']['latitude']
-            r = requests.get('{0}/stores?product=&activity=&latitude={1}&longitude={2}&page=1'.format(server_url, latitude, longitude))
-            #print (r.text)
-            stores = r.json()['_items']
-            if len(stores) == 0:
-                msg = 'Ahora no recuerdo ningún comercio verde por esa zona, pero si me llego a acordar te aviso! 😊'
-            else:
-                if len(stores) == 1:
-                    msg = 'Sé de un comercio verde por tu zona:\n'
-                else:
-                    msg = 'Sé de {0} comercios verdes por tu zona 😃\n'.format(len(stores))
-                for store in stores:
-                    name = store['name']
-                    description =  store['description']
-                    address = store['address']
-                    distance_klm = store['distance_klm']
-                    if distance_klm < 1:
-                        time_bike = '{0} minutos a pie'.format(int(round(distance_klm*9)))
-                    else:
-                        time_bike = '{0} minutos en bicicleta'.format(int(round(180/distance_klm)))
-                    if address == '':
-                        address = '(No recuerdo la dirección 😶, deberás preguntar en el barrio)'
-                    msg = '{0}"{1}"\n{2}\n{3}\n{4}\n\n'.format(msg, name, description, address, time_bike)
-        if 'text' in result['message']:
-            if result['message']['text']:
-                msg = 'Hola! Si me envías tu hubicación te puedo decir que comercios verdes hay por tu zona.\n Si quieres sumar un comercio o necesitas ayuda te invito a unirte a mi grupo:\nhttps://telegram.me/joinchat/0338811e00225f1561463d99065a12d7'
-        r = requests.get('https://api.telegram.org/bot{0}/sendMessage?chat_id={1}&text={2}'.format(bot_token, chat_id, msg))
+    #Bot.bot_loop()
+    try:
+        Bot.bot_loop()
+    except KeyboardInterrupt:
+        break
+    except:
+        print ('Exception')
